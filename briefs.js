01d2db9948bead01ff97e394b51e071ce8ac1b9e
jwaller11 <conguy699@gmail.com>
2026-02-23 13:18:08 -0600

    Create briefs.js

---
+++ briefs.js
@@ -0,0 +1,33 @@
+export const briefs = [
+
+  {
+    id: "fam_vnav_brief",
+    type: "fam",
+    title: "FAM/VNAV BRIEF",
+    steps: [
+      { type: "action", text: "1. Paragraph one verbatim text here." },
+      { type: "action", text: "2. Paragraph two verbatim text here." }
+    ]
+  },
+
+  {
+    id: "inav_brief",
+    type: "inav",
+    title: "INAV BRIEF",
+    steps: [
+      { type: "action", text: "1. Paragraph one verbatim text here." },
+      { type: "action", text: "2. Paragraph two verbatim text here." }
+    ]
+  },
+
+  {
+    id: "forms_brief",
+    type: "form",
+    title: "FORMS BRIEF",
+    steps: [
+      { type: "action", text: "1. Paragraph one verbatim text here." },
+      { type: "action", text: "2. Paragraph two verbatim text here." }
+    ]
+  }
+
+];
