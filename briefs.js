acca9f70e8b3ab79c271cf1b5ffd68ba73ad0567
jwaller11 <conguy699@gmail.com>
2026-02-23 14:00:00 -0600

    Update briefs.js

--- briefs.js
+++ briefs.js
@@ -5,9 +5,25 @@ export const briefs = [
     type: "fam",
     title: "FAM/VNAV BRIEF",
     steps: [
-      { type: "action", text: "1. Paragraph one verbatim text here." },
-      { type: "action", text: "2. Paragraph two verbatim text here." }
-    ]
+      {
+      type: "condition",
+      text: "1. I.M.S.A.F.E. Checklist"
+    },
+    {
+      type: "action",
+      text: "\"I'm safe\""
+    },
+
+    {
+      type: "condition",
+      text: "3. Airsickness History"
+    },
+    {
+      type: "action",
+      text: "Aircrew must announce if they become passively or actively airsick and may pass the controls as the situation dictates. The flying pilot will keep the aircraft in a stable position, minimizing turns as the situation allows. If the airsick pilot feels he or she cannot continue, the mission will be aborted for airsickness."
+    }
+
+  ]
   },
 
   {
