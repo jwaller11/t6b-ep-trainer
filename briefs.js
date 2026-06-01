d39d833c5ecff5774b8bf95b4339715bf55f8b6e
jwaller11 <conguy699@gmail.com>
2026-05-31 19:46:01 -0500

    Update briefs.js

--- briefs.js
+++ briefs.js
@@ -1,49 +1,231 @@
 export const briefs = [
-
   {
-    id: "fam_vnav_brief",
-    type: "fam",
-    title: "FAM/VNAV BRIEF",
-    steps: [
+    "id": "fam_vnav_brief",
+    "type": "fam",
+    "title": "FAM/VNAV BRIEF",
+    "steps": [
       {
-      type: "condition",
-      text: "1. I.M.S.A.F.E. Checklist"
-    },
-    {
-      type: "action",
-      text: "\"I'm safe\""
-    },
-
-    {
-      type: "condition",
-      text: "3. Airsickness History"
-    },
-    {
-      type: "action",
-      text: "Aircrew must announce if they become passively or actively airsick and may pass the controls as the situation dictates. The flying pilot will keep the aircraft in a stable position, minimizing turns as the situation allows. If the airsick pilot feels he or she cannot continue, the mission will be aborted for airsickness."
-    }
-
-  ]
-  },
-
-  {
-    id: "inav_brief",
-    type: "inav",
-    title: "INAV BRIEF",
-    steps: [
-      { type: "action", text: "1. Paragraph one verbatim text here." },
-      { type: "action", text: "2. Paragraph two verbatim text here." }
+        "type": "condition",
+        "text": "1. Airsickness History"
+      },
+      {
+        "type": "action",
+        "text": "Aircrew must announce if they become passively or actively airsick and may pass the controls as the situation dictates. The flying pilot will keep the aircraft in a stable position, minimizing turns as the situation allows. If the airsick pilot feels he or she cannot continue, the mission will be aborted for airsickness"
+      },
+      {
+        "type": "condition",
+        "text": "2. Foreign Object Damage"
+      },
+      {
+        "type": "action",
+        "text": "Ensure only those items required for flight are taken to the aircraft, and that all flight suit pockets are zippered. Both pilots will inspect BOTH cockpits before and after flight to ensure no FOD is present"
+      },
+      {
+        "type": "condition",
+        "text": "3. IMC Penetration"
+      },
+      {
+        "type": "action",
+        "text": "If an IMC penetration is required, the non-flying pilot will call out all airspeed deviations greater than 10 knots, heading deviations greater than 10 degrees, angles of bank greater than 30 degrees, altitude deviations greater than 100’, and if descent rate is greater than current altitude remaining (Minute to Live Rule). Either pilot must announce if they experience any vertigo or spatial disorientation"
+      },
+      {
+        "type": "condition",
+        "text": "4. Approach/Missed Approach"
+      },
+      {
+        "type": "action",
+        "text": "If an instrument approach is required, the IP (or SNA/IUT at the IP’s discretion) will fly the approach to be backed up by the SNA/IUT on all headings, altitudes, airspeeds, angles of bank, and rates of descent. The SNA/IUT will call the runway environment in sight with clock position and repeat the current landing clearance"
+      },
+      {
+        "type": "condition",
+        "text": "5. G-Awareness Procedures"
+      },
+      {
+        "type": "action",
+        "text": "We will conduct a G-Ex prior to conducting any maneuvers requiring greater than three Gs, and preface all maneuvers with ‘Gs coming on, NOW, NOW, NOW.’ Either pilot experiencing gray-out conditions must immediately call ‘Knock-it-Off’ over the ICS and a contact unusual attitude must be used to recover the aircraft. In the event either pilot experiences a GLOC, the training portion of the flight will be terminated, and the IP will recover the aircraft to a suitable airfield"
+      },
+      {
+        "type": "condition",
+        "text": "6. Frequencies:"
+      },
+      {
+        "type": "action",
+        "text": "We will use preset UHF, VHF, and NAV frequencies, and manual frequencies as required"
+      },
+      {
+        "type": "condition",
+        "text": "7. Radio Procedures and Discipline:"
+      },
+      {
+        "type": "action",
+        "text": "The flying pilot will make all radio calls to be backed up by the non-flying pilot. Either pilot can make a safety of flight call. Keep all calls concise and professional"
+      },
+      {
+        "type": "condition",
+        "text": "8. Change of Control of Aircraft"
+      },
+      {
+        "type": "action",
+        "text": "(1) We will use a positive, three-way exchange of controls with emphasis on the word ‘CONTROLS’. In the event of an ICS failure, we will use the ‘Push-to-Pass, Shake-to-Take’ method of control transfer with the non-flying pilot showing his or her hands for verification. If in doubt of who has control of the aircraft, query the other pilot. IP input does not constitute a change of controls unless there is an ICS failure (2) Transfer of aircraft controls includes control of the FMS, UFCP and radios. The nonflying pilot may assist or assume control of the FMS, UFCP and radios as directed"
+      },
+      {
+        "type": "condition",
+        "text": "9. Navigational Aids"
+      },
+      {
+        "type": "action",
+        "text": "We will be primarily VFR today using ground reference checkpoints for navigation. However, we will keep the appropriate working area or navigation route in the FMS for back-up"
+      },
+      {
+        "type": "condition",
+        "text": "10. Clearing Procedures"
+      },
+      {
+        "type": "action",
+        "text": "Both pilots will maintain a vigilant lookout for other traffic using the TCAS and ADS-B to aid as appropriate. Call out all traffic using the clock system, high/level/low, factor/no factor, call a bird a bird, and plane a plane. Any pilot recognizing an immediate traffic conflict will maneuver the aircraft into a safe position and discuss traffic avoidance after it is no longer a factor"
+      },
+      {
+        "type": "condition",
+        "text": "11. Aborts"
+      },
+      {
+        "type": "action",
+        "text": "Either pilot recognizing the need to abort will call ‘ABORT, ABORT, ABORT’ over the ICS. The flying pilot will execute the ABORT procedure IAW NATOPS. If we anticipate departing the prepared surface, we will execute the EMERGENCY ENGINE SHUTDOWN ON THE GROUND procedure. The aircraft commander will call ‘CFS, CFS, CFS’ to command execution of the canopy fracturing system, or ‘EJECT, EJECT, EJECT’ to command ejection as required"
+      },
+      {
+        "type": "condition",
+        "text": "12. Divert Fields"
+      },
+      {
+        "type": "action",
+        "text": "Our primary weather diverts in the local area will be Corpus Christi International (KCRP) and Victoria Regional (KVCT)"
+      },
+      {
+        "type": "condition",
+        "text": "13. Minimum And Emergency Fuel"
+      },
+      {
+        "type": "action",
+        "text": "We will declare MIN FUEL if we anticipate landing below 200 pounds and EMERGENCY FUEL if we anticipate landing below 120 pounds"
+      },
+      {
+        "type": "condition",
+        "text": "14. Loss Of Power"
+      },
+      {
+        "type": "action",
+        "text": "(1) If we have an engine failure shortly after take-off, we will execute the ENGINE FAILURE IMMEDIATELY AFTER TAKEOFF emergency procedure, being mindful of aircraft configuration, energy state, and runway length remaining. If insufficient runway length remains to land straight ahead, we will eject (2) If we have an engine failure elsewhere, we will execute the ENGINE FAILURE DURING FLIGHT emergency procedure. If we are unable to intercept an ELP for a suitable landing site, we will eject"
+      },
+      {
+        "type": "condition",
+        "text": "15. Radio Failure/ICS Failure"
+      },
+      {
+        "type": "action",
+        "text": "(1) In the event of a radio or ICS failure, we will troubleshoot in an attempt to reestablish comms or ICS (i.e. check communications leads all the way to the O2 mask, the comm panel, and UFCP for appropriate frequencies and switchology) (2) If we have a radio failure, we will attempt communication on another radio, using the standby VHF if necessary. If we have a total loss of communications in the local area, we will comply with the local letter of agreement for IFR and VFR aircraft. If we are outside of the local area, we will comply with the FIH (3) If we have an ICS failure, we can remove our masks momentarily and shout or use frequencies 123.45 or 246.8 to communicate as necessary. If ICS cannot be restored, the instructional portion of the flight will be terminated, and we will land as soon as practical"
+      },
+      {
+        "type": "condition",
+        "text": "16. Unintentional Instrument Flight"
+      },
+      {
+        "type": "action",
+        "text": "Instrument conditions are to be avoided at all times while operating VFR. If actual instrument flight is encountered: (a) Maintain aircraft control (b) Establish an instrument scan and check altitude (c) If altitude below Maximum Elevation Figure (MEF) + 1000 feet or Off Route Obstruction Clearance Altitude (OROCA), execute immediate climb to safe altitude turning away from known obstacles. Do not attempt to regain VMC below the calculated or published safe altitude. (d) If altitude above MEF + 1000 feet or OROCA, with smooth coordinated control inputs get out of IMC the way it was entered if climbing→descend (etc.). (e) If unable to regain VMC, the IP will contact the nearest ATC facility and coordinate an IFR clearance. If swift contact cannot be made, Squawk 7700, declare an emergency, and use guard as appropriate"
+      },
+      {
+        "type": "condition",
+        "text": "17. Loss Of Sight/Lost Wingman"
+      },
+      {
+        "type": "action",
+        "text": "We will be single ship today"
+      },
+      {
+        "type": "condition",
+        "text": "18. Downed Pilot and Aircraft"
+      },
+      {
+        "type": "action",
+        "text": "If we are first on scene to an aircraft mishap, we will assume on-scene commander duties. The flying pilot will establish the aircraft at a safe altitude and distance to maintain visual contact and the non-flying pilot will initiate the on-scene commander checklist. We will set a BINGO to the nearest suitable field and remain on-scene until we: 1) Reach our BINGO fuel; 2) Have an emergency of our own; 3) Are relieved by a more appropriate platform; 4) The rescue is complete. If we are not first on scene, we may offer assistance but will remain clear unless called upon"
+      },
+      {
+        "type": "condition",
+        "text": "19. Mid-Air/Airborne Damaged Aircraft/Bird Strike"
+      },
+      {
+        "type": "action",
+        "text": "Our first priority will be to maintain aircraft control. If we are unable to control the aircraft, we will eject. If the aircraft is controllable and we suspect possible engine damage (i.e. within the prop arc), we will execute a PEL to the nearest suitable airfield. If no engine damage is suspected, we will execute a Controllability Check as required."
+      },
+      {
+        "type": "condition",
+        "text": "20. OBOGS Malfunctions/Hypoxia Symptoms"
+      },
+      {
+        "type": "action",
+        "text": "If either member of the crew experiences hypoxic symptoms, or hypoxia is suspected for any reason, both crew members must execute the OBOGS FAILURE/PHYSIOLOGICAL SYMPTOMS procedure"
+      },
+      {
+        "type": "condition",
+        "text": "21. Unsafe Gear"
+      },
+      {
+        "type": "action",
+        "text": "If on departure or recovery, the flying pilot will request entry into the Delta pattern and have tower or RDO inspect the gear while troubleshooting. If a form qualified pilot is in the area, we will coordinate a join up in the Delta pattern to conduct a visual gear inspection"
+      },
+      {
+        "type": "condition",
+        "text": "22. OCF and Spin Recognition and Recovery"
+      },
+      {
+        "type": "action",
+        "text": "(1) “If we are in Out-of-Controlled Flight, we will execute the INADVERTENT DEPARTURE FROM CONTROLLED FLIGHT Emergency Procedure; PCL IDLE, controls neutral, altitude check, recover from unusual altitude (2) “OCF can be identified if the aircraft does not respond immediately and in a normal sense to application of flight controls. Airspeed in a steady-state spin will either be stable or it will oscillate above and below a constant airspeed, while the turn needle will be relied upon to indicate direction of rotation"
+      },
+      {
+        "type": "condition",
+        "text": "23. Other Aircraft Emergencies/Simulated Emergencies"
+      },
+      {
+        "type": "action",
+        "text": "(1) “All emergencies will be treated as actual unless prefaced with the word ‘SIMULATED’. While troubleshooting, we will ensure that one pilot is always flying the aircraft. No fast hands in the cockpit. We will apply the following three rules for all emergencies: 1) Maintain aircraft control. 2) Analyze the situation and take proper action. 3) Land as soon as conditions permit (2) “In the event of an actual malfunction, the pilot recognizing the malfunction will call it out over the ICS. The flying pilot will execute any applicable ‘critical action’ procedures. The non-flying pilot will break out the PCL and review both critical and non-critical action items, as well as all notes, warnings, and cautions. Time permitting, we will get dual concurrence prior to moving the PCL to OFF, pulling the firewall shut-off handle, or switching the PMU to OFF. (3) In the event of a simulated power loss, the IP will initiate by stating, ‘I HAVE THE PCL, SIMULATED’, manipulate the PCL as required to simulate the power loss and set 4- 6 percent torque upon hearing the SNA/IUT verbalize ‘Simulated PCL–OFF.’ In all cases, the flying pilot retains control of the aircraft and may utilize power as necessary (4) For all simulated malfunctions, the SNA/IUT will maintain control of the aircraft and recite the appropriate procedure, moving the landing gear and flap handles as appropriate. Do not move any other switches or handles in a simulated scenario"
+      },
+      {
+        "type": "condition",
+        "text": "24. Ejection"
+      },
+      {
+        "type": "action",
+        "text": "(1) “Ejection is never simulated (2) Immediate. “If impact is imminent with insufficient time for the normal cadence, we will both immediately pull the ejection handle (3) Time Critical (a) “To facilitate proper body position, the call for ejection will normally be a foursecond cadence: ‘EJECT, EJECT, EJECT,’ pull (both) the ejection handle. In the event of an ICS failure, execute three raps on the canopy, pull (both). (b) “Ensure proper body position: back and musters against the seat, head on the head rest, chin up 10 degrees, feet on the rudder pedals, and elbows in tight toward the body. The minimum recommended altitude for uncontrolled ejection is 6,000 feet AGL, and 2,000 feet AGL for a controlled ejection (4) Deliberate. “Time permitting, we will execute as many of the CONTROLLED EJECTION checklist steps as possible. The Controlled Ejection Area is defined as the CRP 170 radial at 20 DME (Chapman Ranch) heading 210"
+      }
     ]
   },
-
   {
-    id: "forms_brief",
-    type: "form",
-    title: "FORMS BRIEF",
-    steps: [
-      { type: "action", text: "1. Paragraph one verbatim text here." },
-      { type: "action", text: "2. Paragraph two verbatim text here." }
+    "id": "inav_brief",
+    "type": "inav",
+    "title": "INAV BRIEF",
+    "steps": [
+      {
+        "type": "action",
+        "text": "1. Paragraph one verbatim text here."
+      },
+      {
+        "type": "action",
+        "text": "2. Paragraph two verbatim text here."
+      }
+    ]
+  },
+  {
+    "id": "forms_brief",
+    "type": "form",
+    "title": "FORMS BRIEF",
+    "steps": [
+      {
+        "type": "action",
+        "text": "1. Paragraph one verbatim text here."
+      },
+      {
+        "type": "action",
+        "text": "2. Paragraph two verbatim text here."
+      }
     ]
   }
-
 ];
