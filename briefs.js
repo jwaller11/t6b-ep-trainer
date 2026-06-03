dfe84d13c619181120aa103a24caec9337d224d6
jwaller11 <conguy699@gmail.com>
2026-06-02 19:50:35 -0500

    Update briefs.js

--- briefs.js
+++ briefs.js
@@ -69,6 +69,10 @@ export const briefs = [
         "text": "(1) We will use a positive, three-way exchange of controls with emphasis on the word ‘CONTROLS’. In the event of an ICS failure, we will use the ‘Push-to-Pass, Shake-to-Take’ method of control transfer with the non-flying pilot showing his or her hands for verification. If in doubt of who has control of the aircraft, query the other pilot. IP input does not constitute a change of controls unless there is an ICS failure (2) Transfer of aircraft controls includes control of the FMS, UFCP and radios. The nonflying pilot may assist or assume control of the FMS, UFCP and radios as directed"
       },
       {
+        "type": "action",
+        "text": "(2) Transfer of aircraft controls includes control of the FMS, UFCP and radios. The nonflying pilot may assist or assume control of the FMS, UFCP and radios as directed"
+      },
+      {
         "type": "condition",
         "text": "Navigational Aids"
       },
@@ -117,12 +121,24 @@ export const briefs = [
         "text": "(1) If we have an engine failure shortly after take-off, we will execute the ENGINE FAILURE IMMEDIATELY AFTER TAKEOFF emergency procedure, being mindful of aircraft configuration, energy state, and runway length remaining. If insufficient runway length remains to land straight ahead, we will eject (2) If we have an engine failure elsewhere, we will execute the ENGINE FAILURE DURING FLIGHT emergency procedure. If we are unable to intercept an ELP for a suitable landing site, we will eject"
       },
       {
+        "type": "action",
+        "text": "(2) If we have an engine failure elsewhere, we will execute the ENGINE FAILURE DURING FLIGHT emergency procedure. If we are unable to intercept an ELP for a suitable landing site, we will eject"
+      },
+      {
         "type": "condition",
         "text": "Radio Failure/ICS Failure"
       },
       {
         "type": "action",
-        "text": "(1) In the event of a radio or ICS failure, we will troubleshoot in an attempt to reestablish comms or ICS (i.e. check communications leads all the way to the O2 mask, the comm panel, and UFCP for appropriate frequencies and switchology) (2) If we have a radio failure, we will attempt communication on another radio, using the standby VHF if necessary. If we have a total loss of communications in the local area, we will comply with the local letter of agreement for IFR and VFR aircraft. If we are outside of the local area, we will comply with the FIH (3) If we have an ICS failure, we can remove our masks momentarily and shout or use frequencies 123.45 or 246.8 to communicate as necessary. If ICS cannot be restored, the instructional portion of the flight will be terminated, and we will land as soon as practical"
+        "text": "(1) In the event of a radio or ICS failure, we will troubleshoot in an attempt to reestablish comms or ICS (i.e. check communications leads all the way to the O2 mask, the comm panel, and UFCP for appropriate frequencies and switchology)"
+      },
+      {
+        "type": "action",
+        "text": "(2) If we have a radio failure, we will attempt communication on another radio, using the standby VHF if necessary. If we have a total loss of communications in the local area, we will comply with the local letter of agreement for IFR and VFR aircraft. If we are outside of the local area, we will comply with the FIH"
+      },
+      {
+        "type": "action",
+        "text": "(3) If we have an ICS failure, we can remove our masks momentarily and shout or use frequencies 123.45 or 246.8 to communicate as necessary. If ICS cannot be restored, the instructional portion of the flight will be terminated, and we will land as soon as practical"
       },
       {
         "type": "condition",
@@ -130,7 +146,27 @@ export const briefs = [
       },
       {
         "type": "action",
-        "text": "Instrument conditions are to be avoided at all times while operating VFR. If actual instrument flight is encountered: (a) Maintain aircraft control (b) Establish an instrument scan and check altitude (c) If altitude below Maximum Elevation Figure (MEF) + 1000 feet or Off Route Obstruction Clearance Altitude (OROCA), execute immediate climb to safe altitude turning away from known obstacles. Do not attempt to regain VMC below the calculated or published safe altitude. (d) If altitude above MEF + 1000 feet or OROCA, with smooth coordinated control inputs get out of IMC the way it was entered if climbing>descend (etc.). (e) If unable to regain VMC, the IP will contact the nearest ATC facility and coordinate an IFR clearance. If swift contact cannot be made, Squawk 7700, declare an emergency, and use guard as appropriate"
+        "text": "Instrument conditions are to be avoided at all times while operating VFR. If actual instrument flight is encountered:"
+      },
+      {
+        "type": "action",
+        "text": "(a) Maintain aircraft control"
+      },
+      {
+        "type": "action",
+        "text": "(b) Establish an instrument scan and check altitude"
+      },
+      {
+        "type": "action",
+        "text": "(c) If altitude below Maximum Elevation Figure (MEF) + 1000 feet or Off Route Obstruction Clearance Altitude (OROCA), execute immediate climb to safe altitude turning away from known obstacles. Do not attempt to regain VMC below the calculated or published safe altitude."
+      },
+      {
+        "type": "action",
+        "text": "(d) If altitude above MEF + 1000 feet or OROCA, with smooth coordinated control inputs get out of IMC the way it was entered if climbing>descend (etc.)."
+      },
+      {
+        "type": "action",
+        "text": "(e) If unable to regain VMC, the IP will contact the nearest ATC facility and coordinate an IFR clearance. If swift contact cannot be made, Squawk 7700, declare an emergency, and use guard as appropriate"
       },
       {
         "type": "condition",
@@ -146,7 +182,23 @@ export const briefs = [
       },
       {
         "type": "action",
-        "text": "If we are first on scene to an aircraft mishap, we will assume on-scene commander duties. The flying pilot will establish the aircraft at a safe altitude and distance to maintain visual contact and the non-flying pilot will initiate the on-scene commander checklist. We will set a BINGO to the nearest suitable field and remain on-scene until we: 1) Reach our BINGO fuel; 2) Have an emergency of our own; 3) Are relieved by a more appropriate platform; 4) The rescue is complete. If we are not first on scene, we may offer assistance but will remain clear unless called upon"
+        "text": "If we are first on scene to an aircraft mishap, we will assume on-scene commander duties. The flying pilot will establish the aircraft at a safe altitude and distance to maintain visual contact and the non-flying pilot will initiate the on-scene commander checklist. We will set a BINGO to the nearest suitable field and remain on-scene until we:"
+      },
+      {
+        "type": "action",
+        "text": "(1) Reach our BINGO fuel;"
+      },
+      {
+        "type": "action",
+        "text": "(2) Have an emergency of our own;"
+      },
+      {
+        "type": "action",
+        "text": "(3) Are relieved by a more appropriate platform;"
+      },
+      {
+        "type": "action",
+        "text": "(4) The rescue is complete. If we are not first on scene, we may offer assistance but will remain clear unless called upon"
       },
       {
         "type": "condition",
@@ -178,7 +230,11 @@ export const briefs = [
       },
       {
         "type": "action",
-        "text": "(1) If we are in Out-of-Controlled Flight, we will execute the INADVERTENT DEPARTURE FROM CONTROLLED FLIGHT Emergency Procedure; PCL IDLE, controls neutral, altitude check, recover from unusual attitude (2) OCF can be identified if the aircraft does not respond immediately and in a normal sense to application of flight controls. Airspeed in a steady-state spin will either be stable or it will oscillate above and below a constant airspeed, while the turn needle will be relied upon to indicate direction of rotation"
+        "text": "(1) If we are in Out-of-Controlled Flight, we will execute the INADVERTENT DEPARTURE FROM CONTROLLED FLIGHT Emergency Procedure; PCL IDLE, controls neutral, altitude check, recover from unusual attitude"
+      },
+      {
+        "type": "action",
+        "text": "(2) OCF can be identified if the aircraft does not respond immediately and in a normal sense to application of flight controls. Airspeed in a steady-state spin will either be stable or it will oscillate above and below a constant airspeed, while the turn needle will be relied upon to indicate direction of rotation"
       },
       {
         "type": "condition",
@@ -186,7 +242,18 @@ export const briefs = [
       },
       {
         "type": "action",
-        "text": "(1) All emergencies will be treated as actual unless prefaced with the word ‘SIMULATED’. While troubleshooting, we will ensure that one pilot is always flying the aircraft. No fast hands in the cockpit. We will apply the following three rules for all emergencies: 1) Maintain aircraft control. 2) Analyze the situation and take proper action. 3) Land as soon as conditions permit (2) In the event of an actual malfunction, the pilot recognizing the malfunction will call it out over the ICS. The flying pilot will execute any applicable ‘critical action’ procedures. The non-flying pilot will break out the PCL and review both critical and non-critical action items, as well as all notes, warnings, and cautions. Time permitting, we will get dual concurrence prior to moving the PCL to OFF, pulling the firewall shut-off handle, or switching the PMU to OFF. (3) In the event of a simulated power loss, the IP will initiate by stating, ‘I HAVE THE PCL, SIMULATED’, manipulate the PCL as required to simulate the power loss and set 4- 6 percent torque upon hearing the SNA/IUT verbalize ‘Simulated PCL–OFF.’ In all cases, the flying pilot retains control of the aircraft and may utilize power as necessary (4) For all simulated malfunctions, the SNA/IUT will maintain control of the aircraft and recite the appropriate procedure, moving the landing gear and flap handles as appropriate. Do not move any other switches or handles in a simulated scenario"
+        "text": "(1) All emergencies will be treated as actual unless prefaced with the word ‘SIMULATED’. While troubleshooting, we will ensure that one pilot is always flying the aircraft. No fast hands in the cockpit. We will apply the following three rules for all emergencies: 1) Maintain aircraft control. 2) Analyze the situation and take proper action. 3) Land as soon as conditions permit"
+      },
+      {
+        "type": "action",
+        "text": "(2) In the event of an actual malfunction, the pilot recognizing the malfunction will call it out over the ICS. The flying pilot will execute any applicable ‘critical action’ procedures. The non-flying pilot will break out the PCL and review both critical and non-critical action items, as well as all notes, warnings, and cautions. Time permitting, we will get dual concurrence prior to moving the PCL to OFF, pulling the firewall shut-off handle, or switching the PMU to OFF."
+      {
+        "type": "action",
+        "text": "(3) In the event of a simulated power loss, the IP will initiate by stating, ‘I HAVE THE PCL, SIMULATED’, manipulate the PCL as required to simulate the power loss and set 4- 6 percent torque upon hearing the SNA/IUT verbalize ‘Simulated PCL–OFF.’ In all cases, the flying pilot retains control of the aircraft and may utilize power as necessary (4) For all simulated malfunctions, the SNA/IUT will maintain control of the aircraft and recite the appropriate procedure, moving the landing gear and flap handles as appropriate. Do not move any other switches or handles in a simulated scenario"
+      },
+        {
+        "type": "action",
+        "text": "(4) For all simulated malfunctions, the SNA/IUT will maintain control of the aircraft and recite the appropriate procedure, moving the landing gear and flap handles as appropriate. Do not move any other switches or handles in a simulated scenario"
       },
       {
         "type": "condition",
